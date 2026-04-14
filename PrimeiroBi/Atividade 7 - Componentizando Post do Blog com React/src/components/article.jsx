e3b7f84a5d48cce29e25a5dcd160e6742d8835a6
felaaass <arturkich6@gmail.com>
2026-04-14 19:34:04 -0300

    atividade 7 certo

--- PrimeiroBi/Atividade 7 - Componentizando Post do Blog com React/src/components/article.jsx
+++
@@ -1,9 +0,0 @@
-export default function article({ titulo, autor, data, conteudo }) {
-  return (
-    <article>
-      <h2>{titulo}</h2>
-      <p>Por {autor} - {data}</p>
-      <div>{conteudo}</div>
-    </article>
-  );
-}
--- PrimeiroBi/Atividade 7 - Componentizando Post do Blog com React/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function footer() {
-  return (
-    <footer>
-      <p>&copy; 2026 Meu Blog. Todos os direitos reservados.</p>
-    </footer>
-  );
-}
